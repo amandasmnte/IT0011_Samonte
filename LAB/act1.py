adb3245b587184fa29c8fc99da9222738d1fe328
amandasmnte <amandanicolesamonte@gmail.com>
2025-01-20 10:49:24 +0800

    01/20/25 1049 - First technical

--- LAB/act1.py
+++ LAB/act1.py
@@ -1,2 +1,27 @@
-name = "Amanda"
-print("My name is: ", name)
+Firstname = input("Enter First Name: ")
+
+Lastname = input("Enter Last Name: ")
+
+Middlename = input("Enter Middle Name: ")
+
+Birthday = input("Enter Birth Date: ")
+
+Gender = input("Enter Gender (Male/Female): ")
+
+Address = input("Enter address: ")
+
+
+Course = input("Enter Course: ")
+
+YearLvl = input("Enter Year Level: ")
+
+print("\nStudent Information \n" )
+print("First Name: ", Firstname)
+print("Last Name: ", Lastname)
+print("Middle Name: ", Middlename)
+print("Birthday: ", Birthday)
+print("Gender: ", Gender)
+print("Address: ", Address)
+print("Course: ", Course)
+print("Year Level: ", YearLvl)
+
